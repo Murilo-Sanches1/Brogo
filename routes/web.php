10fac5f41faf9f296fd0e8a5cbc560fba1da3e29
Murilo-Sanches <sanchesmurilo199@outlook.com>
2022-12-02 10:52:30 -0300

    Preparing the site routes statically

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\Site\HomeController;
+use App\Http\Controllers\Site\CategoryController;
+use App\Http\Controllers\Site\BlogController;
+use App\Http\Controllers\Site\ContactController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +18,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+
+Route::namespace('Site')->group(function () {
+    // Route::get('/', 'HomeController@index');
+    Route::get('/', [HomeController::class, '__invoke']);
+    Route::get('produtos', [CategoryController::class, 'index']);
+    Route::get('produtos/{slug}', [CategoryController::class, 'show']);
+    Route::get('blog', [BlogController::class], '__invoke');
+    Route::view('sobre', 'site.about.index');
+    Route::get('contato', [ContactController::class, 'index']);
+    Route::post('contato', [ContactController::class, 'contactForm']);
 });
